e79e0efd7cda3b4dd5807145e4f4f28cf1ac7a1d
carito <carolina.medina>
2020-02-11 17:20:56 -0300

    botones

---
+++ src/Boton.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import App from './App';
+import './Boton.css';
+
+
+class Boton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            
+            <React.Fragment className="caja">
+                 
+            <input className="cajaTex" placeholder="ingresa texto" id="cajaTexto"></input>
+                
+                <input type="button" className="btnAtras" href="javascript:history.go(-1)" value="Volver Atrás" onclick="history.back()" onClick={()=>{
+               alert("esta en uso")
+                     }} />
+
+                <input type="button" className="btnSiguiente" href="javascript:history.go(1)" value="Siguiente" onclick="history.back()" onClick={()=>{
+               alert("esta en uso")
+                     }} />    
+                
+                <button className="btnGuardar" onClick={()=> this.setState({siguiente: this.state.siguiente +1})}>Guardar</button>
+               
+            </React.Fragment>
+
+        );
+    }
+}
+ 
+export default Boton;
